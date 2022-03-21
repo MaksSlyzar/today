8c52d0473965e817e472e2f59c23cb663ae9d8ce
Vlad1slav <aykoyayko@gmail.com>
2022-03-21 11:37:55 +0200

    qeq

--- src/views/CreateSchedule.vue
+++ src/views/CreateSchedule.vue
@@ -2,7 +2,7 @@
 	<div class="CreateSchedule">
 		<div class="lg-sao-label">Create Schedule</div>
 		<div style="pisition:absolute; top: 1000px; font-family: Verdana; font-size:20px; border-style:groove; border-width: 10px; border-color: orange; padding: 5px">
-      text
+      <div>Add ROZCLAD</div>
 		</div>
 	</div>
 </template>
@@ -19,7 +19,9 @@ export default {
 </script>
 
 <style lang="scss">
+
 .CreateSchedule {
 
 }
+
 </style>
